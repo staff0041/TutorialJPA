be6c9caacd5e7a1ade56607cbbc3e713a27bc409
staff0041 <hkosuteui@hotmail.co.jp>
2024-07-14 13:21:51 +0900

    create_TutorialJPA2

---
+++ src/main/java/com/techacademy/TutorialJpa2Application.java
@@ -0,0 +1,13 @@
+package com.techacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialJpa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialJpa2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/techacademy/TutorialJpa2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialJpa2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
